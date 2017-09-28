2493c5bbac9a0973230960d8482e8a49f7e31dc1
cihan bas <cihanbas07@gmail.com>
2017-09-28 10:52:18 +0300

    status

--- src/Main.js
+++ src/Main.js
@@ -1,38 +1,52 @@
-
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  StyleSheet,  View
+    StyleSheet, View, Text
 } from 'react-native';
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
 
 export default class Main extends Component {
-  render() {
-    return (
-    <Provider store={createStore()}>
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: 'AIzaSyDlfZ71rWcSjdCRkE7y_IGWsgBOWvhRaVs',
+            authDomain: 'ogrencikayit-32ae1.firebaseapp.com',
+            databaseURL: 'https://ogrencikayit-32ae1.firebaseio.com',
+            projectId: 'ogrencikayit-32ae1',
+            storageBucket: 'ogrencikayit-32ae1.appspot.com',
+            messagingSenderId: '364417382182'
+        })
+    }
 
-    </Provider>
-    );
-  }
+    render() {
+        return (
+            <Provider store={createStore(reducers)}>
+                <Text>
+                    Lorem ipsum dolor sit amet.
+                </Text>
+            </Provider>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
 });
 
 
